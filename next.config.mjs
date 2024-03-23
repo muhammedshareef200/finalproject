71d7f4369b6fc4143cd560215bf7d9c55066b138
muhammedshareef200 <128568615+muhammedshareef200@users.noreply.github.com>
2024-03-23 14:38:41 +0530

    Update next -1.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['utfs.io'],
-    remotePatterns: [/^https:\/\/utfs\.io/],
-  },
-};
+  
+    images: {
+      domains: ['utfs.io'],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'utfs.io',
+          port: ''
+        }
+      ]
+    }
+  };
 
 export default nextConfig;
